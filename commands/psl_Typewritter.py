1634c285264ddf180cc03dc201eee3b6058369fd
OdzioM <odziomek91@gmail.com>
2021-12-16 15:23:15 +0100

    drukarka literek, poprawki, detekcja admina

--- commands/psl_Typewritter.py
+++ commands/psl_Typewritter.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 class psl_Typewritter(commands.Cog):
     def __init__(self, client):
@@ -9,24 +10,26 @@ class psl_Typewritter(commands.Cog):
             print(str(e))
 
     @commands.command()
-    async def typewritter(self, ctx, *, arg1):
+    @has_permissions(administrator=True)
+    async def tw(self, ctx, *, arg1):
         # typewritter command
         try:
-            if ctx.message.author.mention == discord.Permissions.administrator:
-                input_text = arg1
-                output_text = f''
-                for char in input_text:
-                    if char.isalpha():
-                        output_text += f':regional_indicator_{char.lower()}: '
-                    else:
-                        output_text += f':blue_square: '
-                await ctx.channel.purge(limit=1)
-                await ctx.channel.send(output_text)
-            else:
-                await ctx.channel.purge(limit=1)
-                embed_title = f'**Brak uprawnień!**\n'
-                embed_description = f'**Nie masz odpowiednich uprawnień do wykonania tej komendy.**\n'
-                embedVar = discord.Embed(title=embed_title, description=embed_description, color=0xff0000)
-                await ctx.channel.send(embed=embedVar)
+            input_text = arg1
+            output_text = f''
+            for char in input_text:
+                if char.isalpha():
+                    output_text += f':regional_indicator_{char.lower()}: '
+                else:
+                    output_text += f':blue_square: '
+            await ctx.channel.purge(limit=1)
+            await ctx.channel.send(output_text)
         except Exception as e:
             print(str(e))
+
+    @tw.error
+    async def tw_error(self, ctx):
+        await ctx.channel.purge(limit=1)
+        embed_title = f'**Brak uprawnień!**\n'
+        embed_description = f'**Nie masz odpowiednich uprawnień do wykonania tej komendy.**\n'
+        embedVar = discord.Embed(title=embed_title, description=embed_description, color=0xff0000)
+        await ctx.channel.send(embed=embedVar)
